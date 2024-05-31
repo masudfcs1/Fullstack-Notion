38fef37d16266d07cd78c5815e3285b62d0337aa
masud rana <masudfcs1@gmail.com>
2024-05-31 23:58:19 +0600

    heros

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Heading from './_components/heading'
+import Heros from './_components/Heros'
 
 const Merketing = () => {
     return (
         <div className=' min-h-full flex flex-col '>
             <div className=' flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10 '>
                 <Heading />
+                <Heros />
             </div>
         </div>
     )

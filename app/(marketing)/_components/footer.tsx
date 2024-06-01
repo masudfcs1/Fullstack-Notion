1934eaf2e7605c96e5e14493c66dbab1cd3fb1a3
masud rana <masudfcs1@gmail.com>
2024-06-01 22:02:48 +0600

    footer

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Logo from './logo'
+import { cn } from '@/lib/utils'
+
 
 const Footer = () => {
   return (
-    <div>footer</div>
+    <div className=' flex items-center w-full p-6 bg-background z-50 '>
+      <Logo />
+    </div>
   )
 }
 
---
+++ app/(marketing)/_components/logo.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Image from 'next/image'
+import { Poppins } from 'next/font/google'
+import { cn } from '@/lib/utils'
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ['400', '600']
+})
+
+export default function Logo() {
+    return (
+        <div className=' hidden md:flex items-center gap-x-2 ' >
+            <Image
+                src='/logo.png'
+                height='30'
+                width='30'
+                alt='logo'
+            />
+            <p className={cn("font-semibold",font.className)} >Shamu</p>
+        </div>
+    )
+}

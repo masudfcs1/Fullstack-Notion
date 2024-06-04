b1754508316bf12ca9b2ece03bc5a9de0d6eb8b6
masud rana <masudfcs1@gmail.com>
2024-06-04 22:28:23 +0600

    scroll and navbar

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+"use client"
+
+import { useScrollTop } from "@/hook/use-scroll-top"
+import { cn } from "@/lib/utils"
+import Logo from "./logo"
 
 const Navbar = () => {
+    const scrolled = useScrollTop()
+
     return (
-        <div>Navbar</div>
+        <div className={cn(" z-50 bg-background fixed w-full top-0 flex items-center p-5", scrolled && "border-b shadow-sm")} >
+            <Logo/>
+            <div className=" md:ml-auto md:justify-end flex justify-between w-full items-center gap-x-2">
+                Login
+            </div>
+        </div>
     )
 }
 

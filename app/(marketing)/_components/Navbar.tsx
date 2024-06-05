0438268598ecec4b52e84b7af156e1ac9a601497
masud rana <masudfcs1@gmail.com>
2024-06-05 22:18:21 +0600

    convex clerk

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -4,14 +4,35 @@ import { ModeToggle } from "@/components/mode-taggle"
 import { useScrollTop } from "@/hook/use-scroll-top"
 import { cn } from "@/lib/utils"
 import Logo from "./logo"
+import { useConvexAuth } from "convex/react"
+import { SignInButton } from "@clerk/clerk-react"
+import { Button } from "@/components/ui/button"
 
 const Navbar = () => {
     const scrolled = useScrollTop()
+    const { isAuthenticated, isLoading } = useConvexAuth()
 
     return (
         <div className={cn(" z-50 bg-background dark:bg-[#1f1f1f] fixed w-full top-0 flex items-center p-5", scrolled && "border-b shadow-sm")} >
             <Logo />
             <div className=" md:ml-auto md:justify-end flex justify-between w-full items-center gap-x-2">
+                {isLoading && (
+                    <p>Loading....</p>
+                )}
+                {!isAuthenticated && !isLoading && (
+                    <>
+                        <SignInButton mode="modal" >
+                            <Button variant='ghost' >
+                                Log In
+                            </Button>
+                        </SignInButton>
+                        <SignInButton mode="modal" >
+                            <Button size='sm' >
+                                Get Shamu free
+                            </Button>
+                        </SignInButton>
+                    </>
+                )}
                 <ModeToggle />
             </div>
         </div>
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://eager-marten-85.clerk.accounts.dev",
+        applicationID: "shamu-notion",
+      },
+    ]
+  };

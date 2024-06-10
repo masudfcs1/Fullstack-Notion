7b305051d201d140c8d50800f90b6781f50d1808
masud rana <masudfcs1@gmail.com>
2024-06-10 08:06:59 +0600

    issue

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -1,17 +1,20 @@
 "use client"
 
-import { ModeToggle } from "@/components/mode-taggle"
-import { useScrollTop } from "@/hook/use-scroll-top"
-import { cn } from "@/lib/utils"
-import { Spinner } from "./spinner"
-import Logo from "./logo"
 import { useConvexAuth } from "convex/react"
+import { SignInButton, UserButton } from "@clerk/clerk-react"
+import Link from "next/link"
+
+import { useScrollTop } from "@/hook/use-scroll-top"
+import { ModeToggle } from "@/components/mode-taggle"
 import { Button } from "@/components/ui/button"
-import { SignInButton } from "@clerk/clerk-react"
+import { Spinner } from "./spinner"
+import { cn } from "@/lib/utils"
+
+import Logo from "./logo"
 
 const Navbar = () => {
-    const scrolled = useScrollTop()
     const { isAuthenticated, isLoading } = useConvexAuth()
+    const scrolled = useScrollTop()
 
     return (
         <div className={cn(" z-50 bg-background dark:bg-[#1f1f1f] fixed w-full top-0 flex items-center p-5", scrolled && "border-b shadow-sm")} >
@@ -34,6 +37,19 @@ const Navbar = () => {
                         </SignInButton>
                     </>
                 )}
+
+                {isAuthenticated && !isLoading && (
+                    <>
+                        <Button variant='ghost' size='sm' asChild >
+                            <Link href='/documents' >
+                                Enter Shamu
+                            </Link>
+                        </Button>
+                        <UserButton afterSignOutUrl="/" />
+                    </>
+                )}
+
+
                 <ModeToggle />
             </div>
         </div>
